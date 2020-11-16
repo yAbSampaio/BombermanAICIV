db59ec402b0c06249371af91a3446fdc89847f9a
Michel Neves <nevess.michel@gmail.com>
2020-11-16 20:04:21 -0300

    dynamic map with players spawn area, lack calculation way player-to-player

--- functions.js
+++ functions.js
@@ -5,16 +5,18 @@
     20: {name:"buff_bomb", code: 2},
     10: {name:"buff_explosion", code: 3}
 };*/
-function randomArbitrary(max = 0, min = 0) {
+function randomArbitrary(min = 0, max = 0) {
   return Math.random() * (max - min) + min;
 }
+
+// check distance between player spawn
 function checkDistance(positions, distance) {
   for (let i = 0; i < positions.length; i++) {
     for (let j = 0; j < positions.length; j++) {
       if (
         i !== j &&
         Math.abs(positions[i].x - positions[j].x) <= distance &&
-        Math.abs(positions[i].y - positions[j].y)
+        Math.abs(positions[i].y - positions[j].y) <= distance
       ) {
         return false;
       }
@@ -22,6 +24,7 @@ function checkDistance(positions, distance) {
   }
   return true;
 }
+
 function sortPlayersPosition(matrix_width, matrix_height, n_players, distance) {
   var positions = new Array(n_players);
   let done = false;
@@ -29,8 +32,8 @@ function sortPlayersPosition(matrix_width, matrix_height, n_players, distance) {
   while (!done) {
     for (var i = 0; i < n_players; i++) {
       positions[i] = {
-        x: Math.round(randomArbitrary(1, matrix_width - 1)),
-        y: Math.round(randomArbitrary(1, matrix_height - 1)),
+        x: Math.round(randomArbitrary(1, matrix_width - 2)),
+        y: Math.round(randomArbitrary(1, matrix_height - 2)),
       };
     }
     if (checkDistance(positions, distance)) {
@@ -40,6 +43,64 @@ function sortPlayersPosition(matrix_width, matrix_height, n_players, distance) {
   return positions;
 }
 
+function calcStartArea(positions, matrix_width, matrix_height) {
+  var spots = {};
+
+  for (let i = 0; i < positions.length; i++) {
+    spots["" + positions[i].x + "," + positions[i].y] = true;
+
+    let count = 0;
+    let directions = [0, 0, 0, 0];
+    while (count < 2) {
+      let random = Math.round(randomArbitrary(0, 3));
+      switch (random) {
+        case 0:
+          if (positions[i].y - directions[0] > 1) {
+            spots[
+              "" + positions[i].x + "," + (positions[i].y - 1 - directions[0])
+            ] = true;
+            directions[0]++;
+            count++;
+          }
+          break;
+        case 1:
+          if (positions[i].x + directions[1] < matrix_width - 2) {
+            spots[
+              "" + (positions[i].x + 1 + directions[1]) + "," + positions[i].y
+            ] = true;
+
+            directions[1]++;
+            count++;
+          }
+          break;
+        case 2:
+          if (positions[i].y + directions[2] < matrix_height - 2) {
+            spots[
+              "" + positions[i].x + "," + (positions[i].y + 1 + directions[2])
+            ] = true;
+
+            directions[2]++;
+            count++;
+          }
+          break;
+        case 3:
+          if (positions[i].x - directions[3] > 1) {
+            spots[
+              "" + (positions[i].x - 1 - directions[3]) + "," + positions[i].y
+            ] = true;
+
+            directions[3]++;
+            count++;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return spots;
+}
+
 function generateMapContent(
   matrix_width,
   matrix_height,
@@ -47,41 +108,37 @@ function generateMapContent(
   blocks_mutable,
   blocks_immutable,
   percentages_range_obj,
-  n_players = 4,
+  spawn_areas,
   tile_edge = 24
 ) {
   var map = new Array(matrix_width);
-  for (var i = 1; i < matrix_width; i++) {
-    map[i] = new Array(matrix_height);
-    for (var j = 1; j < matrix_height; j++) {
-      let percentage = Math.round(Math.random() * 100);
-
-      let uppermost = 0;
-      for (var percentage_range in percentages_range_obj) {
-        if (percentage_range >= percentage) {
-          uppermost = percentage_range;
-          break;
+  for (var i = 0; i < matrix_width; i++) {
+    if (i == 0 || i == matrix_width - 1) {
+      map[i] = new Array(matrix_height).fill(-1);
+    } else {
+      map[i] = new Array(matrix_height);
+      for (var j = 0; j < matrix_height; j++) {
+        if (j == 0 || j == matrix_height - 1) {
+          map[i][j] = -1;
+        } else {
+          let pos_x = i * tile_edge + tile_edge / 2;
+          let pos_y = j * tile_edge + tile_edge / 2;
+          if (spawn_areas["" + i + "," + j]) {
+            grounds.create(pos_x, pos_y, "ground");
+            map[i][j] = 0;
+          } else {
+            let percentage = Math.round(Math.random() * 100);
+            let uppermost = 0;
+            for (var percentage_range in percentages_range_obj) {
+              if (percentage_range >= percentage) {
+                uppermost = percentage_range;
+                break;
+              }
+            }
+            map[i][j] = percentages_range_obj[uppermost].code;
+          }
         }
       }
-      //console.log(percentage, uppermost);
-      let pos_x = i * tile_edge + tile_edge / 2;
-      let pos_y = j * tile_edge + tile_edge / 2;
-      if (percentages_range_obj[uppermost].code === -1) {
-        blocks_immutable.create(
-          pos_x,
-          pos_y,
-          percentages_range_obj[uppermost].name
-        );
-      } else {
-        grounds.create(pos_x, pos_y, "ground");
-      }
-      if (
-        percentages_range_obj[uppermost].code !== 0 &&
-        percentages_range_obj[uppermost].code !== -1
-      ) {
-        blocks_mutable.create(pos_x, pos_y, "block_mutable");
-      }
-      map[i][j] = percentages_range_obj[uppermost].code;
     }
   }
   return map;
